9229a4ea26fabf5736ce430d026385a9df8ad896
vladdezhniuk <vladdezhniuk@gmail.com>
2021-10-23 17:19:59 +0300

    header component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -7,6 +7,14 @@ export const Header = () =>
     <header className="header">
         <div className="header__wrapper">
             <Nav/>
+            <div className="header__text-area">
+                <h1 className="header__title">
+                    Upcoming IGOs__
+                </h1>
+                <p className="header__description">
+                    Full-scale gaming ecosystem for IGOs and NFT pre-sales.
+                </p>
+            </div>
         </div>
     </header>;
 
