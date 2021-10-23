db6e006f41cb81aabeaf607372299d811dce0b91
Bohdan Boiko <bohdan.boiko-external@sema4genomics.com>
2021-10-23 14:17:02 +0300

    init firebase

--- src/modules/Login/index.tsx
+++ src/modules/Login/index.tsx
@@ -1,9 +1,17 @@
 /** just for test rigth now */
 import React from 'react';
+import { getAuth } from 'firebase/auth';
 
 const Login: React.FC = () => {
+    const login = () => {
+        console.log('login');
+        const a = getAuth();
+    };
     return (
-        <h1>Login Page</h1>
+        <>
+            <h1>Login Page</h1>
+            <button onClick={login}>LOGIN</button>
+        </>
     );
 };
 
