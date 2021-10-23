d89fd03034121895a0fd4c18c3e6888bba25132b
Bohdan Boiko <bohdan.boiko-external@sema4genomics.com>
2021-10-23 16:50:56 +0300

    fix host

--- server.ts
+++ server.ts
@@ -6,6 +6,15 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.set('port', process.env.PORT || 8080);
 
+app.get('/some-route', (req, res) => {
+    res.send('ok');
+});
+
+/* final catch-all route to index.html defined last */
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build') + '/index.html');
+});
+
 const server = app.listen(app.get('port'), function () {
     console.log('listening on port ', server.address().port);
 });
