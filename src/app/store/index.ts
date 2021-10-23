9c2a29ba1d98e4b3568ece74b0c461ceb223bd39
Bohdan Boiko <bohdan.boiko-external@sema4genomics.com>
2021-10-23 20:07:17 +0300

    fix routings

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,8 +2,10 @@ import thunk from 'redux-thunk';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 
 import { userReducer } from './reducers/user';
+import { gamesReducer } from './reducers/games';
 const reducer = combineReducers({
-    userReducer,
+    user: userReducer,
+    games: gamesReducer,
 });
 
 export const store = createStore(reducer, applyMiddleware(thunk));
