75c31f70efb84d15364ae6c181ec1a7c1049c1f5
Bohdan Boiko <bohdan.boiko-external@sema4genomics.com>
2021-10-23 15:46:36 +0300

    create login

--- src/modules/Login/index.tsx
+++ src/modules/Login/index.tsx
@@ -1,15 +1,41 @@
 /** just for test rigth now */
 import React from 'react';
-import { getAuth } from 'firebase/auth';
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
+    User,
+} from 'firebase/auth';
+import { firebaseApp, firebaseAuth } from '../../firebase';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Login: React.FC = () => {
-    const login = () => {
-        console.log('login');
-        const a = getAuth();
+    const [user, setUser] = useState<User | null>();
+    const login = async () => {
+        const auth = getAuth();
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth,
+                'test122@gmail.com',
+                'test123'
+            );
+            console.log(user);
+        } catch (e) {
+            console.log(e);
+        }
     };
+
+    useEffect(() => {
+        firebaseAuth.onAuthStateChanged((_user) => {
+            setUser(_user);
+        });
+    }, []);
+
     return (
         <>
             <h1>Login Page</h1>
+            {user?.email}
             <button onClick={login}>LOGIN</button>
         </>
     );
