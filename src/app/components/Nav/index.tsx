48228a6c3493c926fa6dc67ffd50334e8a8b357f
vladdezhniuk <vladdezhniuk@gmail.com>
2021-10-23 18:42:10 +0300

    changed

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -1,1 +1,94 @@
-export * from './Nav';
+
+import logo from '../../static/images/nav/logo.png';
+import medium from '../../static/images/nav/medium.png';
+import twitter from '../../static/images/nav/twitter.png';
+import telegram from '../../static/images/nav/telegram.png';
+import discord from '../../static/images/nav/discord.png';
+import { Cross, DropdownNavBar } from '../../static/images/nav/svg';
+
+import './index.scss';
+import { useState } from 'react';
+
+
+export const Nav = () =>{
+    const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
+    const list = [
+        {
+            icon: telegram,
+            path: "#"
+        },
+        {
+            icon: twitter,
+            path: "#"
+        },
+        {
+            icon: medium,
+            path: "#"
+        },
+        {
+            icon: discord,
+            path: "#"
+        },
+    ]
+    return(
+        <nav className="nav">
+            <img
+                className="nav__logo"
+                src={logo}
+                alt="logo"
+            />
+            <div className="nav__controls">
+            <ul
+                    className="nav__list"
+                >
+                    {list.map((item, index) =>
+                        <li className="nav__item" key={index}>
+                            <a
+                                href={item.path}
+                                className="nav__link"
+                            >
+                                <img className="nav__icon" src={item.icon} alt="" />
+                            </a>
+                        </li>
+
+                    )}
+                </ul>
+                <a href="" className="nav__button">
+                    Login
+                </a>
+                <a href="" className="nav__button">
+                    Register
+                </a>
+            </div>
+            <div className={`nav__controls-mobile${dropdownMenu ? '-active' : ''}`}>
+            <ul
+                    className="nav__list-mobile"
+                >
+                    {list.map((item, index) =>
+                        <li className="nav__item" key={index}>
+                            <a
+                                href={item.path}
+                                className="nav__link"
+                            >
+                                <img className="nav__icon" src={item.icon} alt="" />
+                            </a>
+                        </li>
+
+                    )}
+                </ul>
+                <a href="" className="nav__button-mobile">
+                    Login
+                </a>
+                <a href="" className="nav__button-mobile">
+                    Register
+                </a>
+            </div>
+                <div
+                    className="nav__dropdown"
+                    onClick={() => setDropdownMenu(!dropdownMenu)}
+                >
+                    {dropdownMenu ? <Cross /> : <DropdownNavBar />}
+                </div>
+        </nav>
+    )
+}
