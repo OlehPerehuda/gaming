3393cff4ee591d19b43a0b2f33b4499f03c85554
Oleh Perehuda <peregudson97@gmail.com>
2021-10-24 17:21:09 +0300

    added route in nav for create post page

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -14,112 +14,115 @@ import { SocialList } from "./consts";
 import "./index.scss";
 
 export const Nav = () => {
-  const dispatch = useDispatch();
-  const { email, firstName, image } = useSelector(
-    (state: RootState) => state.user
-  );
-  const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
-  const [userControls, handleControls] = useState<boolean>(false);
+    const dispatch = useDispatch();
+    const { email, firstName, image } = useSelector(
+        (state: RootState) => state.user
+    );
+    const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
+    const [userControls, handleControls] = useState<boolean>(false);
 
-  const handleLogout = () => {
-    dispatch(logoutUser());
-  };
-  const { lang } = useSelector((state: RootState) => state.local);
+    const handleLogout = () => {
+        dispatch(logoutUser());
+    };
+    const { lang } = useSelector((state: RootState) => state.local);
 
-  const handleChangeLang = () => {
-    if (lang === "en") {
-      dispatch(changeLocal("ru"));
-    } else {
-      dispatch(changeLocal("en"));
-    }
-  };
+    const handleChangeLang = () => {
+        if (lang === "en") {
+            dispatch(changeLocal("ru"));
+        } else {
+            dispatch(changeLocal("en"));
+        }
+    };
 
-  return (
-    <nav className="nav">
-      <Link to="/">
-        <img className="nav__logo" src={logo} alt="logo" />
-      </Link>
-      <div className="nav__wrapper">
-        <div className="nav__controls">
-          <ul className="nav__list">
-            {SocialList.map((item, index) => (
-              <li className="nav__item" key={index}>
-                <a href={item.path} className="nav__link">
-                  <img className="nav__icon" src={item.icon} alt="" />
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={`nav__controls-mobile${dropdownMenu ? "-active" : ""}`}>
-          <ul className="nav__list-mobile">
-            {SocialList.map((item, index) => (
-              <li className="nav__item" key={index}>
-                <a href={item.path} className="nav__link">
-                  <img className="nav__icon" src={item.icon} alt="" />
-                </a>
-              </li>
-            ))}
-          </ul>
-          <a href="" className="nav__button-mobile">
-            <FormattedMessage id="login" defaultMessage="Login" />
-          </a>
-          <a href="" className="nav__button-mobile">
-            <FormattedMessage id="register" defaultMessage="Register" />
-          </a>
-        </div>
-        {!email ? (
-          <>
-            <Link to={ERoutes.login} className="nav__button">
-              <FormattedMessage id="login" defaultMessage="Login" />
+    return (
+        <nav className="nav">
+            <Link to="/">
+                <img className="nav__logo" src={logo} alt="logo" />
             </Link>
-            <Link to={ERoutes.registration} className="nav__button">
-              <FormattedMessage id="register" defaultMessage="Register" />
-            </Link>
-          </>
-        ) : (
-          <div className="nav__status">
-            <span className="nav__status__name">Welcome, {firstName}</span>
-            <div
-              className="nav__status__user"
-              onClick={() => handleControls((prev) => !prev)}
-            >
-              {userControls && (
-                <div className="nav__status__user-controls">
-                  <div className="nav__status__logout" onClick={handleLogout}>
-                    Logout
-                  </div>
-                  <Link className="nav__status__logout" to={ERoutes.edit}>
-                    Edit profile
-                  </Link>
-                  <Link className="nav__status__logout" to={ERoutes.home}>
-                    Go to home
-                  </Link>
+            <div className="nav__wrapper">
+                <div className="nav__controls">
+                    <ul className="nav__list">
+                        {SocialList.map((item, index) => (
+                            <li className="nav__item" key={index}>
+                                <a href={item.path} className="nav__link">
+                                    <img className="nav__icon" src={item.icon} alt="" />
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-              )}
-              <img
-                className="nav__status__user__profile"
-                alt="profile"
-                src={image}
-              />
+                <div className={`nav__controls-mobile${dropdownMenu ? "-active" : ""}`}>
+                    <ul className="nav__list-mobile">
+                        {SocialList.map((item, index) => (
+                            <li className="nav__item" key={index}>
+                                <a href={item.path} className="nav__link">
+                                    <img className="nav__icon" src={item.icon} alt="" />
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <a href="" className="nav__button-mobile">
+                        <FormattedMessage id="login" defaultMessage="Login" />
+                    </a>
+                    <a href="" className="nav__button-mobile">
+                        <FormattedMessage id="register" defaultMessage="Register" />
+                    </a>
+                </div>
+                {!email ? (
+                    <>
+                        <Link to={ERoutes.login} className="nav__button">
+                            <FormattedMessage id="login" defaultMessage="Login" />
+                        </Link>
+                        <Link to={ERoutes.registration} className="nav__button">
+                            <FormattedMessage id="register" defaultMessage="Register" />
+                        </Link>
+                    </>
+                ) : (
+                    <div className="nav__status">
+                        <span className="nav__status__name">Welcome, {firstName}</span>
+                        <div
+                            className="nav__status__user"
+                            onClick={() => handleControls((prev) => !prev)}
+                        >
+                            {userControls && (
+                                <div className="nav__status__user-controls">
+                                    <div className="nav__status__logout" onClick={handleLogout}>
+                                        Logout
+                                    </div>
+                                    <Link className="nav__status__logout" to={ERoutes.edit}>
+                                        Edit profile
+                                    </Link>
+                                    <Link className="nav__status__logout" to={ERoutes.home}>
+                                        Go to home
+                                    </Link>
+                                    <Link className="nav__status__logout" to={ERoutes.create}>
+                                        Create post
+                                    </Link>
+                                </div>
+                            )}
+                            <img
+                                className="nav__status__user__profile"
+                                alt="profile"
+                                src={image}
+                            />
+                        </div>
+                    </div>
+                )}
+                <div
+                    className="nav__dropdown"
+                    onClick={() => setDropdownMenu(!dropdownMenu)}
+                >
+                    {dropdownMenu ? <Cross /> : <DropdownNavBar />}
+                </div>
+                <select
+                    className="nav__language"
+                    onChange={handleChangeLang}
+                    value={lang}
+                >
+                    <option value="en">ENG</option>
+                    <option value="ru">RU</option>
+                </select>
             </div>
-          </div>
-        )}
-        <div
-          className="nav__dropdown"
-          onClick={() => setDropdownMenu(!dropdownMenu)}
-        >
-          {dropdownMenu ? <Cross /> : <DropdownNavBar />}
-        </div>
-        <select
-          className="nav__language"
-          onChange={handleChangeLang}
-          value={lang}
-        >
-          <option value="en">ENG</option>
-          <option value="ru">RU</option>
-        </select>
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 };
